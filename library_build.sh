e2a8470ffa777c29712e820aa2f473a6bec4f6a0
James <jammiesprideful@gmail.com>
2023-11-10 21:23:29 -0700

    change build method

--- library_build.sh
+++ library_build.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 
-#Adjust the llama cmakelists file.
-echo "Modifying llamaCpp CMakeLists"
-
-
-# Define the file path
-FILE="llamaCpp/CMakeLists.txt"
-
-# Check if the file exists
-if [ ! -f "$FILE" ]; then
-    echo "Error: File $FILE does not exist."
-    exit 1
-fi
-
-# Create a temporary file
-TEMP_FILE=$(mktemp)
-
-# Use awk to edit the file
-awk '
-    /cmake_minimum_required\(/ {
-        print
-        print "cmake_policy(SET CMP0091 NEW)"
-        next
-    }
-    /set\(CMAKE_C_STANDARD_REQUIRED true\)/ {
-        print
-        print "set(CMAKE_MSVC_RUNTIME_LIBRARY \"MultiThreaded\$<\$<CONFIG:Debug>:Debug>\")"
-        next
-    }
-    { print }
-' "$FILE" > "$TEMP_FILE"
-
-# Replace original file with the modified one
-mv "$TEMP_FILE" "$FILE"
-
-echo "Done modifying"
+##Adjust the llama cmakelists file.
+#echo "Modifying llamaCpp CMakeLists"
+#
+#
+## Define the file path
+#FILE="llamaCpp/CMakeLists.txt"
+#
+## Check if the file exists
+#if [ ! -f "$FILE" ]; then
+#    echo "Error: File $FILE does not exist."
+#    exit 1
+#fi
+#
+## Create a temporary file
+#TEMP_FILE=$(mktemp)
+#
+## Use awk to edit the file
+#awk '
+#    /cmake_minimum_required\(/ {
+#        print
+#        print "cmake_policy(SET CMP0091 NEW)"
+#        next
+#    }
+#    /set\(CMAKE_C_STANDARD_REQUIRED true\)/ {
+#        print
+#        print "set(CMAKE_MSVC_RUNTIME_LIBRARY \"MultiThreaded\$<\$<CONFIG:Debug>:Debug>\")"
+#        next
+#    }
+#    { print }
+#' "$FILE" > "$TEMP_FILE"
+#
+## Replace original file with the modified one
+#mv "$TEMP_FILE" "$FILE"
+#
+#echo "Done modifying"
 
 
 #build llama
@@ -51,7 +51,7 @@ mkdir build
 cd build
 
 echo "Generating build files"
-cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=/c/vcpkg/scripts/buildsystems/vcpkg.cmake -DLLAMA_CUBLAS=ON -DBUILD_SHARED_LIBS=OFF
+cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=/c/vcpkg/scripts/buildsystems/vcpkg.cmake -DLLAMA_CUBLAS=ON -DBUILD_SHARED_LIBS=OFF -DLLAMA_STATIC=ON
 
 echo "Building"
 cmake --build . --config Release
