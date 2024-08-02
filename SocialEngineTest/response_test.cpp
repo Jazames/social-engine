e4136372b21f75de8b38c36d521e24f1d74a80de
James <jammiesprideful@gmail.com>
2024-08-01 23:14:39 -0600

    Round out and fix tests

--- SocialEngineTest/response_test.cpp
+++ SocialEngineTest/response_test.cpp
@@ -13,8 +13,13 @@ namespace slow
         Knowledge k = get_default_knowledge();
 
         //Default personality.
+		InteractionParameters ip;
+		ip.personality = p;
+		ip.appearance = a;
+		ip.knowledge = k;
+		ip.dialogue = "Hello";
 
-        auto response = get_npc_response_synchronous("Hello", a, p, k);
+        auto response = get_npc_response_synchronous(ip);
         auto desired = "Hello.";
         auto undesired = "I'm busy";
         std::cout << "Response: " << response << std::endl;
@@ -34,8 +39,13 @@ namespace slow
         p.morals.care_harm = -1.0;
         p.traits.Compassion = -1.0;
         p.traits.Politeness = -1.0;
+		InteractionParameters ip;
+		ip.personality = p;
+		ip.appearance = a;
+		ip.knowledge = k;
+		ip.dialogue = "Hello";
 
-        auto response = get_npc_response_synchronous("Hello", a, p, k);
+		auto response = get_npc_response_synchronous(ip);
         auto desired = "Go away doofus, you're too blind to realize that you're wasting my time. Now begone.";
         auto undesired = "Hello, I like your hat";
         std::cout << "Response: " << response << std::endl;
@@ -52,8 +62,13 @@ namespace slow
 		Knowledge k = get_default_knowledge();
 
 		//Default personality.
+		InteractionParameters ip;
+		ip.personality = p;
+		ip.appearance = a;
+		ip.knowledge = k;
+		ip.dialogue = "I like your hat";
 
-		auto response = get_npc_response_synchronous("I like your hat", a, p, k);
+		auto response = get_npc_response_synchronous(ip);
 		auto desired = "Thank you.";
 		auto undesired = "I'm busy";
 		std::cout << "Response: " << response << std::endl;
@@ -74,8 +89,13 @@ namespace slow
 		p.traits.Openness = 0.5;
 		p.traits.Politeness = 0.4;
 		p.traits.Compassion = 0.2;
+		InteractionParameters ip;
+		ip.personality = p;
+		ip.appearance = a;
+		ip.knowledge = k;
+		ip.dialogue = "Where can I go to pray?";
 
-		auto response = get_npc_response_synchronous("Where can I go to pray?", a, p, k);
+		auto response = get_npc_response_synchronous(ip);
 		auto desired = " St. Elmo's Chapel.";
 		auto undesired = "I don't know, but I hope you can find some serentiy. There's probably a church nearby. God be with you!";
 		std::cout << "Response: " << response << std::endl;
@@ -96,8 +116,13 @@ namespace slow
 		p.traits.Openness = 0.5;
 		p.traits.Politeness = 0.4;
 		p.traits.Compassion = 0.2;
+		InteractionParameters ip;
+		ip.personality = p;
+		ip.appearance = a;
+		ip.knowledge = k;
+		ip.dialogue = "Where can I find a tincture to heal my headache?";
 
-		auto response = get_npc_response_synchronous("Where can I find a tincture to heal my headache?", a, p, k);
+		auto response = get_npc_response_synchronous(ip);
 		auto desired = "The people at Bellamy's Apothecary should be able to take care of you. They've got all sorts of useful things there.";
 		auto undesired = "I don't know, but I hope you find something. Good luck!";
 		std::cout << "Response: " << response << std::endl;
@@ -119,8 +144,13 @@ namespace slow
 		p.traits.Openness = 0.5;
 		p.traits.Politeness = 0.4;
 		p.traits.Compassion = 0.2;
+		InteractionParameters ip;
+		ip.personality = p;
+		ip.appearance = a;
+		ip.knowledge = k;
+		ip.dialogue = "I need to go on a long journey, where's the best place to get a ride outta here?";
 
-		auto response = get_npc_response_synchronous("I need to go on a long journey, where's the best place to get a ride outta here?", a, p, k);
+		auto response = get_npc_response_synchronous(ip);
 		auto desired = "Take a steam train from Whitfield Station.";
 		auto undesired = "I don't know, but I hope you can get to where you're going. Good luck!";
 		std::cout << "Response: " << response << std::endl;
